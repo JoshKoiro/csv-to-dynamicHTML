75fcb3ab73bc0b841cabe61fc517c144b7c86905
Josh Koiro <jkoiro@rvii.com>
2016-08-11 13:33:00 -0400

    Corrected tester.js to read ./source-js/data.js

--- tester.js
+++ tester.js
@@ -1,6 +1,6 @@
-var test = require('./json/test.js');
+var test = require('./source-js/data.js');
 
 var data = test.data();
 for (i = 0; i < data.length;i++){
-  console.log(data[i].Name + " is " + data[i].Age + " years old!");
+  console.log(data[i].Wo + " is " + data[i].Customer + " years old!");
 }
